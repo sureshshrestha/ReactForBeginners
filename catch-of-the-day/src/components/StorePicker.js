b641247cc4465f62c7bbb347bf188ebe4b669745
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-11-29 15:23:14 +0545

    5. loading css into our react application.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,6 @@ class StorePicker extends React.Component {
   render() {
     return (
     <form className="store-selector">
-      {/*    look here */}
       <h2>Please enter store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">Visit store -></button>
