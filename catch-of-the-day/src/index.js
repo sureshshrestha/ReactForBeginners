59d2f9fa42c584057caa9db84bfe38bfc9ce0070
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-11-29 16:43:05 +0545

    6. Creating our application layout with components.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './css/style.css';
-import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
