43be9d2c6e01be7d954feb35775e32e7aaa90abf
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-11-30 11:03:01 +0545

    9. Routing with React Router.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render () {
-    console.log(this);
-    return (
-      <header className="top">
-        <h1>Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          Day</h1>
-        <h3 className="tagline"><span>{this.props.tagline}       </span></h3>
-      </header>
-    )
-  }
+const Header = (props) => {
+  return (
+    <header className="top">
+      <h1>
+        Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        Day</h1>
+      <h3 className="tagline"><span>{props.tagline}       </span></h3>
+    </header>
+  )
 }
+    console.log(this);
+
 
 export default Header;
