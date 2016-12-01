692149b8f4ceff579847d84fa9233b533a934ded
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-12-01 13:37:20 +0545

    20. Bi-directonal Data flow and Live state editing.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
     // getintitalstate
@@ -56,7 +57,13 @@ class App extends React.Component {
     //set state
     this.setState({fishes});
   }
-  
+
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({fishes});
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -90,7 +97,12 @@ class App extends React.Component {
           order={this.state.order}
           params={this.props.params}
         />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     )
   }
