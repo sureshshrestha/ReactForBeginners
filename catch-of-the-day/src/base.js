ea7d51257b1d9588a9432eead3776cd084c540bc
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-12-01 12:45:40 +0545

    18. Persisting our State with Firebase.

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyC998GhPznP61fcPVj0FJ8_rD5Dh--AUx8",
+  authDomain: "catch-of-the-day-walker.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-walker.firebaseio.com"
+});
+
+export default base;
